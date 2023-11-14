bd0273e43be066e03c57956ddf4fdf1f833a0ecd
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-14 17:17:42 -0500

    fixed server side bug connecting to mongo

--- backEnd/index.js
+++ backEnd/index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/employee")
+mongoose.connect("mongodb://127.0.0.1:27017/employee")
 
 app.post('/register', (req, res) =>{
     EmployeeModel.create(req.body)
